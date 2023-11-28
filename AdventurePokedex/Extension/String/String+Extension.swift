469b469848277f768f42e215d2d381ffc71e4a90
Abraham Vazquez <mabraham.vazquez@gmail.com>
2023-11-28 00:54:52 -0600

    [ADD] Adding Networking class, update protocols, and Extension

---
+++ AdventurePokedex/Extension/String/String+Extension.swift
@@ -0,0 +1,13 @@
+//  String+Extension.swift
+//  AdventurePokedex
+//  Created by Abraham Vazquez on 28/11/23.
+
+
+import Foundation
+
+extension String: PathsProtocol {
+    var strPathToUse: String {
+        return self
+    }
+
+}
--- AdventurePokedex/Modules/AllPokemon/Protocols/AllPokemonProtocols.swift
+++ AdventurePokedex/Modules/AllPokemon/Protocols/AllPokemonProtocols.swift
@@ -104,3 +104,8 @@ protocol AllPokemon_PresenterToViewProtocol: AnyObject {
 // MARK: PRESENTER -> ROUTER
 protocol AllPokemon_PresenterToRouterProtocol: AnyObject {
 }
+
+
+protocol PathsProtocol {
+    var strPathToUse: String { get }
+}
