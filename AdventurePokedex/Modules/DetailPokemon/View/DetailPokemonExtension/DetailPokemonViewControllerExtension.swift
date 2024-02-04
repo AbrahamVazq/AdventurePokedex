b1e7136ec6d4ee036bfb0bdac39a41a227f8754b
Abraham Vazquez <mabraham.vazquez@gmail.com>
2024-02-04 14:30:27 -0600

    [UPDATE] Adding new Screen to view detail of Pokemon

---
+++ AdventurePokedex/Modules/DetailPokemon/View/DetailPokemonExtension/DetailPokemonViewControllerExtension.swift
@@ -0,0 +1,8 @@
+//
+//  DetailPokemonViewControllerExtension.swift
+//  AdventurePokedex
+//
+//  Created by Abraham Vazquez on 18/01/24.
+//
+
+import Foundation
---
+++ AdventurePokedex/View/CollectionViewCell/PokemonSpriteCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonSpriteCollectionViewCell.swift
+//  AdventurePokedex
+//
+//  Created by Abraham Vazquez on 18/01/24.
+//
+
+import UIKit
+
+class PokemonSpriteCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
