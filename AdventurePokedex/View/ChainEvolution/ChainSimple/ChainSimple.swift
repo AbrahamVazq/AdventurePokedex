544632cf64bcf7ab4e8e448e6841c6a004211866
Abraham Vazquez <mabraham.vazquez@gmail.com>
2025-01-21 12:54:39 -0600

    [UPDATE] Fix on UIView from 2 Evolution Steps

--- AdventurePokedex/View/ChainEvolution/ChainSimple/ChainSimple.swift
+++ AdventurePokedex/View/ChainEvolution/ChainSimple/ChainSimple.swift
@@ -7,14 +7,21 @@ import UIKit
 class ChainSimple: UIView {
     //MARK: - O U T L E T S
     @IBOutlet weak var vwContainer: UIView!
-    @IBOutlet private weak var lblLevel: UILabel!
-    @IBOutlet private weak var lblNamePokemon: UILabel!
-    @IBOutlet private weak var imgPokemon: UIImageView!
+    @IBOutlet private weak var lblTitleOne: UILabel!
+    @IBOutlet private weak var lblTitleTwo: UILabel!
+    @IBOutlet private weak var lblLevelOne: UILabel!
+    @IBOutlet private weak var lblLevelTwo: UILabel!
+    @IBOutlet private weak var imgPkmnOne: UIImageView!
+    @IBOutlet private weak var imgPkmnTwo: UIImageView!
+    @IBOutlet private weak var imgArrow : UIImageView!
     
     override func awakeFromNib() { }
     
     class func instantiate(with obj: DetailPokemonChain) -> ChainSimple {
-        if let nib = Bundle.main.loadNibNamed(ChainSimple.identifier,  owner: self, options: nil)?[0] as? ChainSimple { return nib }
+        if let nib = Bundle.main.loadNibNamed(ChainSimple.identifier,  owner: self, options: nil)?.first as? ChainSimple {
+            nib.lblTitleOne.text = obj.name?.first
+            nib.lblTitleTwo.text = obj.name?.last
+            return nib }
         return ChainSimple()
     }
 
